7757c0d678d1a811d3cb3dd1473c108e16afe8e4
Chris <7539871+paxperscientiam@users.noreply.github.com>
2020-01-04 17:18:20 -0500

    bump

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-export default function<T, K extends Extract<keyof NonNullable<T>, string> | string | Array<string|number>, L>(obj: T, keys: K, val: L): void {
-  // tslint:disable-next-line
+export default function<T, K extends Extract<keyof NonNullable<T>, string> | string | Array<string|number>, L>(obj: T & Record<string, any>, keys: K|string[], val: L): void {
   (keys as string).split &&
-    (keys = ((keys as string).split(".") as string[]))
+    (keys = ((keys as string).split(".")) as string[])
   const l: number = keys.length
   let i: number = 0
   let t = obj
   let x
+
   for (; i < l; ++i) {
     x = t[keys[i]]
-    t = t[keys[i]] = (i === l - 1 ? val : (x != null ? x :
-                                           (((!!~keys[i + 1] as unknown) as string).indexOf(".") || !(+keys[i + 1] > -1)) ? {} : []))
+    t = (t[(keys[i] as any)] as unknown) = ((i === l - 1 ? val : (x != null ? x :
+                                                                  (((!!~keys[i + 1] as unknown) as string).indexOf(".") || !(+keys[i + 1] > -1)) ? {} : [])) as any)
   }
 }
