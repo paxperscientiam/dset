9e2dec7ac4b011732af365fc4e923bd27f3fb056
Chris <7539871+paxperscientiam@users.noreply.github.com>
2020-01-04 14:54:49 -0500

    some improvements

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-export default function<T, K extends Extract<keyof NonNullable<T>, string>, L>(obj: T, keys: K, val: L) {
-  keys.split && (keys=keys.split("."))
+export default function<T, K extends Extract<keyof NonNullable<T>, string> | string | Array<string|number>, L>(obj: T, keys: K, val: L): void {
+  // tslint:disable-next-line
+  (keys as string).split &&
+    (keys = ((keys as string).split(".") as string[]))
   const l: number = keys.length
-  let i = 0
+  let i: number = 0
   let t = obj
   let x
   for (; i < l; ++i) {
     x = t[keys[i]]
-    t = t[keys[i]] = (i === l - 1 ? val : (x != null ? x : (!!~keys[i+1].indexOf('.') || !(+keys[i+1] > -1)) ? {} : []))
+    t = t[keys[i]] = (i === l - 1 ? val : (x != null ? x :
+                                           (((!!~keys[i + 1] as unknown) as string).indexOf(".") || !(+keys[i + 1] > -1)) ? {} : []))
   }
 }
